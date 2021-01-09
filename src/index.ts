7d881ebd933989e6a77fce94ee115d388840701f
Adeyemi Onibokun <adeyemi.onibokun@decagon.dev>
2021-01-09 22:31:02 +0100

    Fixed issues

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../client/build/')));
 app.use('/', userRouter);
 app.use('/', productRouter);
 app.use('/', cloudinaryRouter);
