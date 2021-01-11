7e33a3f220f79dccae147669c913e1cccf2d7245
Adeyemi Onibokun <adeyemi.onibokun@decagon.dev>
2021-01-11 14:39:23 +0100

    Fix

--- dist/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    SECRET: '@QEGTUI',
-    CLOUDINARY_NAME: 'dtof2ye7q',
-    api_key: '876189335417452',
-    api_secret: 'VhblpK2zk3OQoqtDKnmgtcSWR_M',
-};
